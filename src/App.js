1e89a286f5e69415aeacbb4d40d86cd10f2f47c9
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-08 23:05:18 +0000

    Set x and y in state to move canvas rectangle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const canvasRef = useRef(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+
+    context.fillRect(x, y, 100, 100);
+  }, [x, y]);
+
   return (
     <div className="app">
-      <canvas />
+      <canvas ref={canvasRef} />
 
       <div className="arrows">
-        <button>Up</button>
-        <button>Left</button>
-        <button>Down</button>
-        <button>Right</button>
+        <button onClick={() => setY((y) => y - 20)}>Up</button>
+        <button onClick={() => setX((x) => x - 20)}>Left</button>
+        <button onClick={() => setY((y) => y + 20)}>Down</button>
+        <button onClick={() => setX((x) => x + 20)}>Right</button>
       </div>
 
       <div className="images">
