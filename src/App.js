6e057e32d8ddbc26a4aec5fe1fbff49b0aea2d2e
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-08 23:28:11 +0000

    Add moves by key presses

--- src/App.js
+++ src/App.js
@@ -12,17 +12,36 @@ export default function App() {
     context.canvas.width = window.innerWidth;
 
     context.fillRect(x, y, 100, 100);
+
+    // add event listener to window to listen for arrow keys
+    window.addEventListener('keydown', handleKeyDown);
+
+    function handleKeyDown(e) {
+      if (e.key === 'ArrowUp') move('up');
+      if (e.key === 'ArrowDown') move('down');
+      if (e.key === 'ArrowLeft') move('left');
+      if (e.key === 'ArrowRight') move('right');
+    }
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
   }, [x, y]);
+
+  function move(direction) {
+    if (direction === 'up') setY((y) => y - 20);
+    if (direction === 'down') setY((y) => y + 20);
+    if (direction === 'left') setX((x) => x - 20);
+    if (direction === 'right') setX((x) => x + 20);
+  }
 
   return (
     <div className="app">
       <canvas ref={canvasRef} />
 
       <div className="arrows">
-        <button onClick={() => setY((y) => y - 20)}>Up</button>
-        <button onClick={() => setX((x) => x - 20)}>Left</button>
-        <button onClick={() => setY((y) => y + 20)}>Down</button>
-        <button onClick={() => setX((x) => x + 20)}>Right</button>
+        <button onClick={() => move('up')}>Up</button>
+        <button onClick={() => move('left')}>Left</button>
+        <button onClick={() => move('down')}>Down</button>
+        <button onClick={() => move('right')}>Right</button>
       </div>
 
       <div className="images">
